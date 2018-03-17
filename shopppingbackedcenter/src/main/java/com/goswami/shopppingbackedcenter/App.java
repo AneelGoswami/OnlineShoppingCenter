b061bbbbdd90f94bb75a1d3e8ea0ba22dbeb48aa
Aneel <Aneel@Goswami>
2018-03-17 09:21:29 +0500

    My first spring mvc project

---
+++ shopppingbackedcenter/src/main/java/com/goswami/shopppingbackedcenter/App.java
@@ -0,0 +1,13 @@
+package com.goswami.shopppingbackedcenter;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
